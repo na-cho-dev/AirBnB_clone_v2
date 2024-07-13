f47a1a48b3edb4219b7a26ac99ce8fa112ea8e57
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-13 17:09:36 +0100

    Task 5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,63 +1,91 @@
-# Setup the web servers for the deployment of web_static
-exec { '/usr/bin/env apt -y update' : }
--> package { 'nginx':
-  ensure => installed,
-}
--> file { '/data':
+# Configures a web server for deployment of web_static.
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /etc/nginx/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
   ensure  => 'directory'
-}
--> file { '/data/web_static':
+} ->
+
+file { '/data/web_static':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases':
+} ->
+
+file { '/data/web_static/releases':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases/test':
+} ->
+
+file { '/data/web_static/releases/test':
   ensure => 'directory'
-}
--> file { '/data/web_static/shared':
+} ->
+
+file { '/data/web_static/shared':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases/test/index.html':
+} ->
+
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>"
-}
--> file { '/data/web_static/current':
+  content => "Holberton School Puppet\n"
+} ->
+
+file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-}
--> exec { 'chown -R ubuntu:ubuntu /data/':
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
--> file { '/var/www':
+
+file { '/etc/nginx':
   ensure => 'directory'
-}
--> file { '/var/www/html':
+} ->
+
+file { '/etc/nginx/html':
   ensure => 'directory'
-}
--> file { '/var/www/html/index.html':
+} ->
+
+file { '/etc/nginx/html/index.html':
   ensure  => 'present',
-  content => "<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    <p>Nginx server test</p>
-  </body>
-</html>"
-}
-exec { 'nginx_conf':
-  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
-  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
-  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
-}
--> service { 'nginx':
-  ensure => running,
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/etc/nginx/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
