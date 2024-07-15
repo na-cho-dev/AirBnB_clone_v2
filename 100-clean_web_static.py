ac5954787f50a30e1ddc9d963b7dda099bd30715
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-15 02:14:43 +0100

    Task 4. Keep it clean!

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # Fabfile to delete out-of-date archives.
-import os
 from fabric.api import *
 
 
@@ -17,15 +16,14 @@ def do_clean(number=0):
     number is 2, keeps the most and second-most recent archives,
     etc.
     """
-    number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+    number = int(number)
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+
+    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    path = '/data/web_static/releases'
+    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
