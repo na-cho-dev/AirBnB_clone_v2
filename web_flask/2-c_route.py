64c624370753ef9242f92417d266258e760b2b41
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-29 05:02:34 +0100

    Task 2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -10,17 +10,18 @@ def hello_hbnb():
     """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Displays 'HBNB'."""
     return "HBNB"
+
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return "C {}".format(text)
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
