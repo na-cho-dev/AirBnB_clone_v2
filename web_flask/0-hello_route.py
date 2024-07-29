ec48ab20a0bb609d6a1bdf90456251170ac5baa3
Fortune Iheanacho <nachodev369@gmail.com>
2024-07-29 04:42:46 +0100

    Task 0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,5 +11,6 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
