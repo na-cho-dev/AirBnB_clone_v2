96af11b4e86261f5181720612d337d2f2fa4de9a
Fortune Iheanacho <nachodev369@gmail.com>
2024-08-02 10:33:17 +0100

    Update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,23 +6,28 @@ Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """Display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def close_db(exc):
+    """close the current session of sqlalchemist"""
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+@app.route('/states_list')
+def states_list():
+    """
+    Displays an HTML page with a list of all State objects in DBStorage.
+    States are sorted by name.
+    """
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
